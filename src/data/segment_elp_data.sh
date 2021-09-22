8c7a92ea503da9c1fd1683cce8a90a4ec45cd491
Lucy Tan <lucyktan@gmail.com>
2021-09-22 14:47:49 -0400

    Update file path

--- src/data/segment_elp_data.sh
+++ src/data/segment_elp_data.sh
@@ -25,4 +25,4 @@ EOF
 export AZURE_CONNECTION_STRING='DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;'
 
 
-pipenv shell "python test_segment_files.py 30"
+pipenv shell "python ../segment_elp_data.py 30"
